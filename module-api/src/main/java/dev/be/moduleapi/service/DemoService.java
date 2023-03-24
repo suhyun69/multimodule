470eb39e0ef9042afd776fa1ccf119924a9d123d
suhpark <suhpark@gmarket.com>
2023-03-24 21:58:23 +0900

    Bean을 사용한 모듈간 의존성 테스트

--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,12 +1,19 @@
 package dev.be.moduleapi.service;
 
 import dev.be.modulecommon.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.modulecommon.service.CommonDemoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DemoService {
+
+    private final CommonDemoService commonDemoService;
+
     public String save() {
         System.out.println(CodeEnum.SUCCESS.getCode());
+        System.out.println(commonDemoService.commonService());
         return "save";
     }
 
