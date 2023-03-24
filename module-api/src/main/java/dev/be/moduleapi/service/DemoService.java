c1a2f4243a561915ba8c9c4d6f847c8575c7592e
suhpark <suhpark@gmarket.com>
2023-03-24 21:51:25 +0900

    Java 클래스를 사용한 모듈간 의존성 테스트

---
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -0,0 +1,16 @@
+package dev.be.moduleapi.service;
+
+import dev.be.modulecommon.modulecommon.enums.CodeEnum;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    public String save() {
+        System.out.println(CodeEnum.SUCCESS.getCode());
+        return "save";
+    }
+
+    public String find() {
+        return "find";
+    }
+}
